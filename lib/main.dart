d3d82fafc3ecadd5091bb9b9c472eaaa4b2f5382
patricia.damasceno <patricia.damasceno>
2020-09-03 16:19:42 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue
+        )
+      ),
+    );
+  }
+}
